63abbc0f8cd23acb2de8b5b671d48140beba3f0b
Ruan Pearce-Authers <ruanpa@outlook.com>
2023-08-26 11:40:16 +0100

    Don't export modules

--- wasm-init/src/lib.rs
+++ wasm-init/src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(target_family = "wasm")]
-pub mod wasm;
+mod wasm;
 #[cfg(target_family = "wasm")]
 pub use wasm::*;
 
 #[cfg(not(target_family = "wasm"))]
-pub mod stub;
+mod stub;
 #[cfg(not(target_family = "wasm"))]
 pub use stub::*;
